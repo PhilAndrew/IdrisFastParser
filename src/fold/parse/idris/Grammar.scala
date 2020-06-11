a9bfef6d4df0d36d3a583e2bd089bd9f7350d7e2
Philip Andrew <philip368320@gmail.com>
2020-06-11 22:11:57 +1000

    Minor update

--- src/fold/parse/idris/Grammar.scala
+++ src/fold/parse/idris/Grammar.scala
@@ -6,22 +6,50 @@ import fastparse.P
 object Grammar {
   import fastparse._
 
-  def methodParameters[_ : P] = P ( aFewIdentifiers ~ optionalParameter.rep )
-  def optionalParameter[_: P] = P ( maybeSpace ~ "->" ~ maybeSpace ~ aFewIdentifiers )
-  def methodDecl[_: P] = P ( identifier_t ~ maybeSpace ~ ":" ~ maybeSpace ~ methodParameters)
+  def method[_: P] = P ( methodDecl ~ optSpace ~ newLine ~ methodImpl ~ optSpace ~ newLine)
 
-  def aFewIdentifiers[_: P] = P(option1 | option2 | option3)
+  def newLine[_: P] = P("\n")
 
-  def option1[_: P] = P(Lexical.identifier ~ P(someSpace ~ Lexical.identifier).rep(0))
-  def option2[_: P] = P("(" ~ maybeSpace ~ Lexical.identifier ~ P(someSpace ~ Lexical.identifier).rep(0) ~ maybeSpace ~ ")")
-  def option3[_: P] = P("{" ~ maybeSpace ~ Lexical.identifier ~ P(someSpace ~ Lexical.identifier).rep(0) ~ maybeSpace ~ "}")
+  def methodParameters[_ : P] = P ( methodParameter ~ optionalParameter.rep )
+  def optionalParameter[_: P] = P ( optSpace ~ "->" ~ optSpace ~ methodParameter )
+  // Method definition
+  def methodDecl[_: P] = P ( identifier_t ~ optSpace ~ ":" ~ optSpace ~ methodParameters)
+  // Method implementation
+  // eg. reverse xs = revAcc [] xs where
+  def methodImpl[_: P] = P ( identifiers ~ space ~ "=" ~ methodCall ~ P(space ~ "where") )
 
-  def docComment_t[_: P] = P ( "|||" )
+  // Calling a method
+  def methodCall[_: P] = P ( identifiersInMethodCall )
+  def identifiersInMethodCall[_: P] = P(identifierInMethodCall) ~ P(space ~ identifierInMethodCall).rep(0)
+  def identifierInMethodCall[_: P] = P(emptyArray | Lexical.identifier)
+
+  def methodParameter[_: P] = P(identifiers | option2 | option3)
+
+  def identifiers[_: P] = P(Lexical.identifier ~ P(space ~ Lexical.identifier).rep(0))
+  def option2[_: P] = P("(" ~ optSpace ~ Lexical.identifier ~ P(space ~ Lexical.identifier).rep(0) ~ optSpace ~ ")")
+  def option3[_: P] = P("{" ~ optSpace ~ Lexical.identifier ~ P(space ~ Lexical.identifier).rep(0) ~ optSpace ~ "}")
+
+
+
+
   def identifier_t[_: P] = Lexical.identifier
 
-  def maybeSpace[_: P] = CharIn(" ").rep(0)
-  def someSpace[_: P] = CharIn(" ").rep(1)
+  def optSpace[_: P] = CharIn(" ").rep(0)
+  def space[_: P] = CharIn(" ").rep(1)
 
-  def moduleHeader[_: P] = P( docComment_t.? ~ "module" ~ someSpace ~ identifier_t.? ~ maybeSpace ~ ";" )
+  def moduleHeader[_: P] = P( "module" ~ space ~ identifier_t ~ optSpace ~ ";" )
 
+  //-----------------------------
+
+  def importLine[_: P] = P ( "import" ~ space ~ identifier_t ~ optSpace ~ ";" )
+
+
+  // Matching an empty array such as [] or [ ]
+  def emptyArray[_: P] = P ( "[" ~ optSpace ~ "]" ).!
+
+  // Documentation
+
+  def SingleLineComment_t[_: P] = P( "--" ~ CharsWhile(_ != '\n', 0) )
+  def DocCommentLine[_: P] = P ( "|||" ~ CharsWhile(_ != '\n', 0) )
+  def ArgCommentLine[_: P] = P ( "|||" ~ optSpace ~ "@" ~ CharsWhile(_ != '\n', 0) )
 }
