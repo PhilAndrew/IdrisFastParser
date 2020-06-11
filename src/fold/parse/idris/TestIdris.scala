0b75f98655e577977189f154cecaedf0798e0994
Philip Andrew <philip368320@gmail.com>
2020-06-11 22:12:23 +1000

    Minor update

--- src/fold/parse/idris/TestIdris.scala
+++ src/fold/parse/idris/TestIdris.scala
@@ -1,5 +1,7 @@
 package fold.parse.idris
 
+import java.nio.file.{Files, Path}
+
 object TestIdris extends App {
   import fastparse._
 
@@ -8,12 +10,34 @@ object TestIdris extends App {
     val result = parse(str, Grammar.moduleHeader(_))
     println(result)
   }
+  testModule
 
   def testMethod = {
     val str = "reverse : {List1 a} -> ( List2 b c) -> List3"
     val result = parse(str, Grammar.methodDecl(_))
     println(result)
   }
-
   testMethod
+
+  def testImport = {
+    val str = "import identifier;"
+    val result = parse(str, Grammar.importLine(_))
+    println(result)
+  }
+  testImport
+
+  def testMethodCall = {
+    val str = "revAcc [] xs where"
+    val result = parse(str, Grammar.methodCall(_))
+    println(result)
+  }
+  testMethodCall
+
+  /*
+  def testIdrisFile(fileName: String) = {
+    val str = Files.readString(Path.of("resources/idrisfiles/test01.idr"))
+    println(str)
+  }
+  testIdrisFile("fileName")
+*/
 }
